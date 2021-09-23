3dbbea0f9af2ffc58a83cc40b9000f661b3f9419
Lasindu Nuwanga <lasindunuwanga@gmail.com>
2021-09-24 00:22:47 +0530

    lasindu project was initialized

---
+++ app/src/main/java/com/example/coffee_lovers_mrcoffee/CustomerChangePasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.coffee_lovers_mrcoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_change_password);
+    }
+}
