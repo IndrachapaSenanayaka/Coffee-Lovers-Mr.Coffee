819197bbfd727150acfe2206e89e47cf3c218766
Lasindu Nuwanga <lasindunuwanga@gmail.com>
2021-09-26 14:36:42 +0530

    display customer details

--- app/src/main/java/com/example/coffee_lovers_mrcoffee/services/AuthService.java
+++ app/src/main/java/com/example/coffee_lovers_mrcoffee/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.coffee_lovers_mrcoffee.services;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.coffee_lovers_mrcoffee.data.models.Customer;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -9,25 +10,65 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.Objects;
+
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public class AuthService {
 
     private final FirebaseAuth fAuth;
     private final FirebaseFirestore fireStore;
 
+    private final String KEY_COLLECTION_CUSTOMERS = "customers";
+
+    public PublishSubject<Customer> currentUser = PublishSubject.create();
+
     public AuthService() {
         fAuth = FirebaseAuth.getInstance();
         fireStore = FirebaseFirestore.getInstance();
+
+        TrackCurrentUser();
+    }
+
+
+    /**
+     * Start tracking changes of current user
+     */
+    private void TrackCurrentUser() {
+
+        FirebaseUser fUser = fAuth.getCurrentUser();
+
+        if (fUser == null) {
+            currentUser.onNext(null);
+        } else {
+
+            fireStore.collection(KEY_COLLECTION_CUSTOMERS)
+                    .document(fUser.getUid())
+                    .addSnapshotListener((value, error) -> {
+                        if (error != null) {
+                            System.err.println("Current user error: " + error);
+                        } else {
+                            currentUser.onNext(value.toObject(Customer.class));
+                        }
+
+                        System.out.println("Current user changes: " + (fUser == null ? "NULL" : fUser.getUid()));
+                    });
+
+        }
+
     }
 
 
     /**
      * Sign up a new user using Email and Password
      */
-    public void SignUp(Customer customer, OnCompleteListener onComplete, OnFailureListener onFailure) {
+    public void SignUp(@NonNull Customer customer, OnCompleteListener onComplete, OnFailureListener onFailure) {
 
         // auth using firebase auth
         fAuth.createUserWithEmailAndPassword(customer.email, customer.password)
@@ -36,8 +77,10 @@ public class AuthService {
                         customer.id = Objects.requireNonNull(task.getResult()).getUser().getUid();
                         CreateUserData(customer, task1 -> {
                             if (task1.isSuccessful()) {
+                                TrackCurrentUser();
                                 onComplete.onComplete(Tasks.forResult(null));
                             } else {
+                                fAuth.signOut();
                                 onFailure.onFailure(task1.getException());
                             }
                         });
@@ -54,7 +97,7 @@ public class AuthService {
      */
     private void CreateUserData(Customer cus, OnCompleteListener onComplete) {
 
-        fireStore.collection("customers")
+        fireStore.collection(KEY_COLLECTION_CUSTOMERS)
                 .document(cus.id)
                 .set(cus)
                 .addOnCompleteListener(onComplete);
@@ -69,8 +112,9 @@ public class AuthService {
 
         fAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
-                    if(task.isSuccessful()) {
+                    if (task.isSuccessful()) {
                         onComplete.onComplete(Tasks.forResult(null));
+                        TrackCurrentUser();
                     } else {
                         onFailure.onFailure(task.getException());
                     }
--- app/src/main/java/com/example/coffee_lovers_mrcoffee/ui/customer/CustomerProfileActivity.java
+++ app/src/main/java/com/example/coffee_lovers_mrcoffee/ui/customer/CustomerProfileActivity.java
@@ -3,14 +3,73 @@ package com.example.coffee_lovers_mrcoffee.ui.customer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.coffee_lovers_mrcoffee.Container;
 import com.example.coffee_lovers_mrcoffee.R;
+import com.example.coffee_lovers_mrcoffee.data.models.Customer;
+import com.example.coffee_lovers_mrcoffee.services.AuthService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
 
 public class CustomerProfileActivity extends AppCompatActivity {
+
+    // dependencies
+    private final Container container = Container.instant();
+    private final AuthService authService = container.authService;
+
+    // components
+    private TextView txt_firstName, txt_lastName, txt_email, txt_phone, txt_birthday;
+
+    // disposals
+    private Disposable currentUserDisposer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_profile);
+
+        // init components
+        txt_firstName = findViewById(R.id.txt_profile_firstName);
+        txt_lastName = findViewById(R.id.txt_profile_lastName);
+        txt_email = findViewById(R.id.txt_profile_email);
+        txt_phone = findViewById(R.id.txt_profile_phone);
+        txt_birthday = findViewById(R.id.txt_profile_birthday);
+
+        // listen for current user changes
+        currentUserDisposer = authService
+                .currentUser
+                .subscribe(this::ListenUserChanges);
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // remove listners
+        currentUserDisposer.dispose();
+    }
+
+
+    // ---- EVENTS
+
+    // current user changes listners
+    private void ListenUserChanges(Customer customer) {
+
+        // update fields
+        txt_firstName.setText(customer.firstName);
+        txt_lastName.setText(customer.lastName);
+        txt_email.setText(customer.email);
+        txt_phone.setText(customer.phoneNumber);
+        txt_birthday.setText(DateTimeFormatter.ISO_LOCAL_DATE.format(LocalDate.parse(customer.birthday.toString())));
+
+    }
+
 }
