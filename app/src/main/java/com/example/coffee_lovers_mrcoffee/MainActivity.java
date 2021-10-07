aec055e01fc90cf1605cd4d4b4286d8f3d5bf215
senanayakeindrachapa <senanayakeindrachapa@gmail.com>
2021-10-07 20:13:29 +0530

    add promotion is done

--- app/src/main/java/com/example/coffee_lovers_mrcoffee/MainActivity.java
+++ app/src/main/java/com/example/coffee_lovers_mrcoffee/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.coffee_lovers_mrcoffee;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.text.Html;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_menu);
         setContentView(R.layout.activity_main);
-        
+
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CustomerViewPromotionsList.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CustomerViewPromotionsList.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CustomOrder.class);
+                startActivity(intent);
+            }
+        });
     }
 }
